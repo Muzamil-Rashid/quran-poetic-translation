7c5c580764def286c59162d91d68319600be7e2b
Muzamil Rashid <muzamilmatta63@gmail.com>
2026-01-31 23:29:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const backBtn = document.getElementById("backBtn");
  * SURAHS (114) + ABOUT
  **************************************/
 const surahs = [
-  { name: "About Adfar", type: "about", pdf: "Data/About.pdf" },
+  { name: "About Adfar", type: "about", pdf: "Data/About this book.pdf" },
 
   { name: "Surah Al-Fatiha", pdf: "pdfs/surahs/al-fatiha.pdf" },
   { name: "Surah Al-Baqarah", pdf: "pdfs/surahs/al-baqrah.pdf" },
@@ -121,3 +121,4 @@ backBtn.onclick = () => {
   listSection.style.display = "block";
   pdfViewer.innerHTML = "";
 };
+
